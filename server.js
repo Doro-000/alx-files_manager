f0d42aa4cfdff7f95ab2a2a478e42dcafe0112d0
Hizkyas Tekle <hizkyastekle@gmail.com>
2022-02-04 16:55:04 +0300

    redis and mongo connections working, api functional

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { env } from 'process';
+import bodyParser from 'body-parser';
+import router from './routes/index';
+
+const port = env.PORT ? env.PORT : 5000;
+const app = express();
+app.use(bodyParser.json());
+app.use(router);
+app.listen(port, '127.0.0.1');
+
+export default app;
