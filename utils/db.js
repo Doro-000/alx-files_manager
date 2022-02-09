08417cfe9869efc3fa42b2f670af495af45467fa
Hizkyas Tekle <hizkyastekle@gmail.com>
2022-02-09 17:03:53 +0300

    debugging

--- utils/db.js
+++ utils/db.js
@@ -14,7 +14,7 @@ export class DBClient {
     const host = env.DB_HOST ? env.DB_HOST : '127.0.0.1';
     const port = env.DB_PORT ? env.DB_PORT : 27017;
     const database = env.DB_DATABASE ? env.DB_DATABASE : 'files_manager';
-    this.myClient = MongoClient(`mongodb://${host}:${port}/${database}`, { useUnifiedTopology: true });
+    this.myClient = MongoClient(`mongodb://${host}:${port}/${database}`);
     this.myClient.connect();
   }
 
