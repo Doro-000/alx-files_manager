1ab5e3a7509410246e13cef9a331b159eaa4f940
Philimon Derib <58902914+philimon-reset@users.noreply.github.com>
2022-02-09 18:42:33 +0300

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,8 @@
+import { Buffer } from 'buffer';
 import { env } from 'process';
 import { MongoClient, ObjectID } from 'mongodb';
 import { createHash } from 'crypto';
 import { v4 } from 'uuid';
-import { open, mkdir } from 'fs/promises';
 
 export class DBClient {
   static SHA1(str) {
